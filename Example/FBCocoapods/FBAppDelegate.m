3bc7e9e8f8439a9a0b84cc64fee89a182a728511
yuanfengbo <yuanfengbo@zuoyebang.com>
2021-05-07 19:40:12 +0800

    m

--- Example/FBCocoapods/FBAppDelegate.m
+++ Example/FBCocoapods/FBAppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    // master
+    // master_master
     return YES;
 }
 
